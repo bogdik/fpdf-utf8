ceb4402ca363a4ed7c68e1e59af7e5fc9271ac1c
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2017-12-08 15:15:53 +0100

    Replaced PDF barcodes 39 and 128 scripts

---
+++ bin/TestPDF128.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * TestPDF128
+ *
+ * Generates a Code 128 Barcode PDF document
+ *
+ * @author Tim Peters <info@faimmedia.nl>
+ * @copyright FaimMedia B.V. 2017
+ */
+
+use FaimMedia\FPDF\PDFBarcode;
+
+define('ROOT_PATH', realpath(__DIR__.'/..').'/');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$fpdf = new PDFBarcode();
+$fpdf->setCachePath(ROOT_PATH.'cache/font/');
+$fpdf->setFontPath(ROOT_PATH.'src/font/');
+$fpdf->AddPage();
+$fpdf->Code128(5, 5, '1234566700345', 50, 50);
+
+header('Content-Type: application/pdf; charset=UTF-8');
+echo $fpdf->output();
---
+++ bin/TestPDF39.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * TestPDF39
+ *
+ * Generates a Code 39 Barcode PDF document
+ *
+ * @author Tim Peters <info@faimmedia.nl>
+ * @copyright FaimMedia B.V. 2017
+ */
+
+use FaimMedia\FPDF\PDFBarcode;
+
+define('ROOT_PATH', realpath(__DIR__.'/..').'/');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$fpdf = new PDFBarcode();
+$fpdf->setCachePath(ROOT_PATH.'cache/font/');
+$fpdf->setFontPath(ROOT_PATH.'src/font/');
+$fpdf->AddPage();
+$fpdf->Code39(5, 5, '1234566700345');
+
+header('Content-Type: application/pdf; charset=UTF-8');
+echo $fpdf->output();
